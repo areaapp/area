0bbadb17717dc00967836011cbb929a56c0aa9e0
Cedric Legendre <cedric.legendre@epitech.eu>
2020-01-21 10:43:39 +0100

    (init) [web]: Initialize web client configuration

--- area-web/.eslintrc.js
+++ area-web/.eslintrc.js
@@ -1,17 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  // add your custom rules here
-  rules: {
-  }
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
+    ],
+    // add your custom rules here
+    rules: {
+        "vue/html-indent": ["warn", 4],
+        "strict": "off",
+        "class-methods-use-this": "off",
+        "global-require": "off",
+        "no-param-reassign": ["error", { "props": false }],
+        "no-console": ["warn", { allow: ["warn", "error"] }],
+        "indent": ["warn", 4],
+        "semi": "warn",
+        "vue/singleline-html-element-content-newline": "off",
+        "vue/require-prop-types": "off"
+    },
+
+    "overrides": [
+        {
+            "files": ["*.vue"],
+            "rules": {
+                "indent": "off",
+                "vue/script-indent": "off"
+            }
+        }
+    ]
+};
