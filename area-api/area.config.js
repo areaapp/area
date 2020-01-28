84efefa78472483b51a1703ab7747c965073c183
Jonathan Lemoine <jonathan.lemoine@epitech.eu>
2020-01-28 16:24:01 +0100

    (feat) [action/reaction]: Add some actions and reactions

--- area-api/area.config.js
+++ area-api/area.config.js
@@ -10,25 +10,62 @@ module.exports = {
         reactions: []
     },
     google: {
-        actions: [{
-            name: 'google_gmail_new_mail',
-            displayName: 'New email on Gmail',
-            description: 'Triggered when a email is received in gmail',
-            params: {
+        actions: [
+            {
+                name: 'google_gmail_new_mail',
+                displayName: 'New email on Gmail',
+                description: 'Triggered when a email is received in gmail',
+                params: {
+                }
+            },
+            {
+                name: 'google_youtube_new_video',
+                displayName: 'New video uploaded on a Youtube channel',
+                description: 'Triggered when a video is uploaded on youtube channel',
+                params: {
+                    channel: 'string'
+                }
             }
-        }],
-        reactions: [{
-            name: 'google_gmail_send_email',
-            displayName: 'Send email with Gmail',
-            description: 'Send email with Gmail',
-            params: {
-                addresses: 'Array',
-                content: 'string'
+        ],
+        reactions: [
+            {
+                name: 'google_gmail_send_email',
+                displayName: 'Send email with Gmail',
+                description: 'Send email with Gmail',
+                params: {
+                    addresses: 'Array',
+                    content: 'string'
+                }
+            },
+            {
+                name: 'google_youtube_add_to_watch_later',
+                displayName: 'Add to watch later',
+                description: 'Add a video to "watch later" playlist',
+                params: {
+                    video: 'string'
+                }
             }
-        }]
+        ]
     },
     office: {
-        actions: [],
+        actions: [
+            {
+                name: 'spotify_new_artist_song',
+                displayName: 'New song',
+                description: 'Triggered when a new song is added for an artist on Spotify',
+                params: {
+                    artist: 'string'
+                }
+            },
+            {
+                name: 'spotify_artist_appear_in_new_song',
+                displayName: 'Artist appear in a new song',
+                description: 'Triggered when an artist appear in a new song',
+                params: {
+                    artist: 'string'
+                }
+            }
+        ],
         reactions: []
     },
     spotify: {
@@ -36,7 +73,25 @@ module.exports = {
         reactions: []
     },
     twitch: {
-        actions: [],
+        actions: [
+            {
+                name: 'twitch_streamer_connected',
+                displayName: 'Streamer connected',
+                description: 'Triggered when a streamer is connected',
+                params: {
+                    streamer: 'string'
+                }
+            },
+            {
+                name: 'twitch_game_has_x_viewers',
+                displayName: 'Game has X viewers',
+                description: 'Triggered when a game exceeds X viewers on Twitch',
+                params: {
+                    game: 'string',
+                    viewers: 'integer'
+                }
+            }
+        ],
         reactions: []
-    },
+    }
 };
