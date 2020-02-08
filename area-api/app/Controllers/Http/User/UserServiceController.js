8864417c54d7222e97b67818fe10c35cc2b0095e
Jonathan Lemoine <jonathan.lemoine@epitech.eu>
2020-02-08 23:14:12 +0100

    (feat) [api]: Add error handling to delete service

--- area-api/app/Controllers/Http/User/UserServiceController.js
+++ area-api/app/Controllers/Http/User/UserServiceController.js
@@ -8,97 +8,88 @@ const OAuthController = use('App/Controllers/Http/Auth/OAuthController');
 class UserServiceController {
 
     async addService({ auth, params, request, response }) {
-        try {
-            const paramNames = ['authCode', 'clientType'];
-            const parameters = request.only(paramNames);
+        const paramNames = ['authCode', 'clientType'];
+        const parameters = request.only(paramNames);
 
-            for (let paramName of paramNames) {
-                if (typeof parameters[paramName] === 'undefined') {
-                    return response.status(400).json({
-                        status: 'error',
-                        message: paramName + ' invalid'
-                    });
-                }
-            }
-
-            if (!(params.serviceName in Services)) {
-                return response.status(404).json({
-                    status: 'error',
-                    message: 'Invalid service'
-                });
-            }
-
-            const service = Services[params.serviceName];
-            const accessToken = await OAuthController.getAccessToken(
-                service,
-                parameters.authCode,
-                parameters.clientType
-            );
-
-            if (accessToken === null) {
+        for (let paramName of paramNames) {
+            if (typeof parameters[paramName] === 'undefined') {
                 return response.status(400).json({
                     status: 'error',
-                    message: 'Cannot retreive access_token'
+                    message: paramName + ' invalid'
                 });
             }
-
-            const serviceUser = await service.getUser(accessToken);
-
-            const userService = await Service.query()
-                  .where('email', serviceUser.email)
-                  .where('name', params.serviceName)
-                  .first();
-
-            if (userService !== null) {
-                return response.status(400).json({
-                    status: 'error',
-                    message: 'This email for this service is already added by another user'
-                });
-            }
-            const serviceInfos = {
-                name: params.serviceName,
-                oauth_token: accessToken,
-                oauth_refresh_token: null,
-                email: serviceUser.email,
-            };
-
-            const newService = await auth.current.user.services().create(serviceInfos);
-
-            return response.json({
-                status: 'success',
-                data: newService
-            });
-        } catch (err) {
-            console.log(err);
         }
+
+        if (!(params.serviceName in Services)) {
+            return response.status(404).json({
+                status: 'error',
+                message: 'Invalid service'
+            });
+        }
+
+        const service = Services[params.serviceName];
+        const accessToken = await OAuthController.getAccessToken(
+            service,
+            parameters.authCode,
+            parameters.clientType
+        );
+
+        if (accessToken === null) {
+            return response.status(400).json({
+                status: 'error',
+                message: 'Cannot retreive access_token'
+            });
+        }
+
+        const serviceUser = await service.getUser(accessToken);
+
+        const userService = await Service.query()
+              .where('email', serviceUser.email)
+              .where('name', params.serviceName)
+              .first();
+
+        if (userService !== null) {
+            return response.status(400).json({
+                status: 'error',
+                message: 'This email for this service is already added by another user'
+            });
+        }
+        const serviceInfos = {
+            name: params.serviceName,
+            oauth_token: accessToken,
+            oauth_refresh_token: null,
+            email: serviceUser.email,
+        };
+
+        const newService = await auth.current.user.services().create(serviceInfos);
+
+        return response.json({
+            status: 'success',
+            data: newService
+        });
     }
 
     async getUserServices({auth, response}) {
-        try {
-            const userService = await Service.query()
-            .where('user_id', auth.current.user.id)
-            .fetch();
+        const userService = await Service.query()
+              .where('user_id', auth.current.user.id)
+              .fetch();
 
-            const userServicesInfos = userService.toJSON();
-            let services = {};
+        const userServicesInfos = userService.toJSON();
+        let services = {};
 
-            for (var i = 0; i < userServicesInfos.length; i++) {
-                let service = {
-                    name: userServicesInfos[i].name,
-                    email: userServicesInfos[i].email
-                };
+        for (var i = 0; i < userServicesInfos.length; i++) {
+            let service = {
+                name: userServicesInfos[i].name,
+                email: userServicesInfos[i].email
+            };
 
-                services[service.name] = service;
-            }
-
-            return response.json({
-                status: 'success',
-                data: services
-            });
+            services[service.name] = service;
         }
-        catch (err) {
-            console.log(err);
-        }
+
+        return response.json({
+            status: 'success',
+            data: services
+        });
     }
 
     async deleteService({auth, params, response}) {
@@ -107,20 +98,31 @@ class UserServiceController {
         .where('name', params.name)
         .fetch();
 
-        if (userService.rows.length == 0)
+        if (userService.rows.length == 0) {
             return response.status(404).json({
                 status: 'error',
                 message: 'The service doesn\'t exist for this user'
             });
+        }
 
-        await Service.query()
-        .where('user_id', auth.current.user.id)
-        .where('name', params.name)
-        .delete();
+        const serviceInfos = {
+            name: userService.rows[0].name,
+            email: userService.rows[0].email
+        };
+
+        try {
+            await userService.rows[0].delete();
+        } catch (err) {
+            console.log(err);
+            return response.status(400).json({
+                status: 'error',
+                message: 'Unable to delete service'
+            });
+        }
 
         return response.json({
             status: 'success',
-            message: 'The service ' + params.name + ' is suppressed'
+            data: serviceInfos
         });
     }
 }
