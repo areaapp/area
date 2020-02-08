273fcd29753c36bf68eea9065b7c762d5c78ebe5
Cedric Legendre <cedric.legendre@epitech.eu>
2020-02-08 18:29:58 +0100

    (fix) [web-client]: (WIP) Fix fonts

--- area-web/nuxt.config.js
+++ area-web/nuxt.config.js
@@ -15,8 +15,7 @@ export default {
             { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ]
     },
     /*
