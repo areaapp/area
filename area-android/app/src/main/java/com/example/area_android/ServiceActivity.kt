33b33e86780341280fdbd3adf29ac737f200aeea
Jonathan Lemoine <jonathan.lemoine@epitech.eu>
2020-02-11 17:36:37 +0100

    (feat) [services]: List actions and reactions of the service in the service view

--- area-android/app/src/main/java/com/example/area_android/ServiceActivity.kt
+++ area-android/app/src/main/java/com/example/area_android/ServiceActivity.kt
@@ -2,15 +2,14 @@ package com.example.area_android
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.json.responseJson
 import com.github.kittinunf.result.Result
+
 
 class ServiceActivity : AppCompatActivity() {
 
@@ -21,12 +20,14 @@ class ServiceActivity : AppCompatActivity() {
         val app = this.application as AreaApplication
         val serviceName = intent.getStringExtra("service")
 
-        val name = findViewById<TextView>(R.id.serviceName)
         val desc = findViewById<TextView>(R.id.serviceDescription)
 
         val add = findViewById<Button>(R.id.add)
         val modify = findViewById<Button>(R.id.modify)
         val delete = findViewById<Button>(R.id.delete)
+
+        val actionList = findViewById<ListView>(R.id.actions)
+        val reactionList = findViewById<ListView>(R.id.reactions)
 
         val url = app.serverUrl + "/services/" + serviceName
         Fuel.get(url)
@@ -36,8 +37,44 @@ class ServiceActivity : AppCompatActivity() {
                     }
                     is Result.Success -> {
                         val data = result.get().obj().getJSONObject("data")
-                        name.text = data.getString("displayName")
+
+                        this.supportActionBar!!.title = data.getString("displayName")
                         desc.text = data.getString("description")
+                        val actionTexts: MutableList<Map<String, String>> = ArrayList()
+
+                        val actions = data.getJSONArray("actions")
+                        for (i in 0 until actions.length()) {
+                            val action = actions.getJSONObject(i)
+                            val datum: MutableMap<String, String> = HashMap()
+                            datum["First Line"] = action.getString("displayName")
+                            datum["Second Line"] = action.getString("description")
+                            actionTexts.add(datum)
+                        }
+
+                        val actionAdapter = SimpleAdapter(this,
+                            actionTexts,
+                            android.R.layout.simple_list_item_2,
+                            arrayOf("First Line", "Second Line"),
+                            intArrayOf(android.R.id.text1, android.R.id.text2)
+                        )
+                        actionList.adapter = actionAdapter
+
+                        val reactions = data.getJSONArray("actions")
+                        for (i in 0 until actions.length()) {
+                            val reaction = reactions.getJSONObject(i)
+                            val datum: MutableMap<String, String> = HashMap()
+                            datum["First Line"] = reaction.getString("displayName")
+                            datum["Second Line"] = reaction.getString("description")
+                            actionTexts.add(datum)
+                        }
+
+                        val reactionAdapter = SimpleAdapter(this,
+                            actionTexts,
+                            android.R.layout.simple_list_item_2,
+                            arrayOf("First Line", "Second Line"),
+                            intArrayOf(android.R.id.text1, android.R.id.text2)
+                        )
+                        reactionList.adapter = reactionAdapter
                     }
                 }
             }
@@ -59,6 +96,10 @@ class ServiceActivity : AppCompatActivity() {
 
         add.setOnClickListener{
             this.redirectToService(serviceName)
+        }
+
+        modify.setOnClickListener {
+            Toast.makeText(this, "Not implemented yet", Toast.LENGTH_SHORT).show()
         }
 
         delete.setOnClickListener {
