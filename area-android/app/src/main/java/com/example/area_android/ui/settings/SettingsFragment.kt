ae4f0f51c16c0ab9c3aa34c9cc33cd431be913fc
Jonathan Lemoine <jonathan.lemoine@epitech.eu>
2020-02-11 19:12:51 +0100

    (feat) [user]: Add user icon

--- area-android/app/src/main/java/com/example/area_android/ui/settings/SettingsFragment.kt
+++ area-android/app/src/main/java/com/example/area_android/ui/settings/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -15,6 +16,7 @@ import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.json.responseJson
 import com.github.kittinunf.result.Result
+import com.lelloman.identicon.view.GithubIdenticonView
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -33,7 +35,9 @@ class SettingsFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
 
         val app : AreaApplication = this.activity!!.application as AreaApplication
-
+        val email = root.findViewById<TextView>(R.id.settings_email_text)
+        val username = root.findViewById<TextView>(R.id.settings_username_text)
+        val avatar = root.findViewById<GithubIdenticonView>(R.id.avatar)
 
         val url : String = app.serverUrl + "/me"
         Fuel.get(url)
@@ -48,9 +52,10 @@ class SettingsFragment : Fragment() {
                     is Result.Success -> {
                         val obj : JSONObject = result.get().obj()
                         val data : JSONObject = obj.getJSONObject("data")
-                        print("SUCCESS SETT FRAGMENT data : " + result.get().obj().toString() + "  \n")
-                        root.findViewById<TextView>(R.id.settings_email_text).text = "Email : " + data.getString("email")
-                        root.findViewById<TextView>(R.id.settings_username_text).text = "Username : " + data.getString("username")
+
+                        email.text = "Email : " + data.getString("email")
+                        username.text = data.getString("username")
+                        avatar.hash = data.getString("email_md5").hashCode()
                     }
                 }
             }
