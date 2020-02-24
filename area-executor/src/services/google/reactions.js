db0be9cb1ac9f76fee8b24c7c5276436ce12f89c
Jonathan Lemoine <jonathan.lemoine@epitech.eu>
2020-02-24 14:12:05 +0100

    (fix) [google]: Fix send email with gmail

--- area-executor/src/services/google/reactions.js
+++ area-executor/src/services/google/reactions.js
@@ -8,26 +8,23 @@ Content-Type: text/html; charset="us-ascii"\n\
 MIME-Version: 1.0\n\
 Content-Transfer-Encoding: 7bit\n\
 to: ${area.reaction.args.to}\n\
-from: ${area.user.email}\n\
+from: ${area.reaction.service.email}\n\
 subject: ${area.reaction.args.subject}\n\n\
 <html><body>\
 ${area.reaction.args.content}\
 </body></html>`;
 
-        console.log(emailFormat);
-        const route = `https://www.googleapis.com/gmail/v1/users/${area.user.email}/messages/send`;
+        const route = `https://www.googleapis.com/gmail/v1/users/me/messages/send`;
 
         const email = Base64.encodeURI(emailFormat);
-
-        console.log(email);
 
         await ctx._axios.post(route, {
             raw: email
         }, {
             headers: {
                 'Content-Type': 'application/json',
-                Accept: 'application/json',
-                Authorization: `Bearer ${area.reaction.service.oauth_token}`
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${area.reaction.service.oauth_token}`
             }
         });
     }
